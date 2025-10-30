df503da3bcee24fbfe8307e6206a818af9a2e720
CloudsCloudsClouds <56483061+CloudsCloudsClouds@users.noreply.github.com>
2025-10-30 14:01:48 -0400

    Movement working!

--- scripts/player_nini.gd
+++ scripts/player_nini.gd
@@ -16,17 +16,30 @@ var state: STATE = STATE.MOVEMENT
 func _ready() -> void:
 	player.axis_lock_angular_x = true
 	player.axis_lock_angular_z = true
+	player.axis_lock_angular_y = true
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(_delta: float) -> void:
 	var wish_dir := Vector3.ZERO
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	
-	if Input.is_action_pressed("move_forward"):
-		player.wish_dir = -phantom_camera_3d.transform.basis.z.normalized()
-	if Input.is_action_pressed("move_backward"):
-		player.wish_dir = phantom_camera_3d.transform.basis.z.normalized()
-	if Input.is_action_pressed("move_left"):
-		player.wish_dir = -phantom_camera_3d.transform.basis.x.normalized()
-	if Input.is_action_pressed("move_right"):
-		player.wish_dir = phantom_camera_3d.transform.basis.x.normalized()
+
+	if state == STATE.MOVEMENT:
+		var input_rot := phantom_camera_3d.get_third_person_rotation().y
+		wish_dir = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, input_rot)
+		wish_dir = wish_dir.normalized() * 0.5
+		player.wish_dir = wish_dir
+
+		animation_tree.set(bs2d, Vector2(input_dir.x, input_dir.y))
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var pcam_rot_deg: Vector3
+
+		pcam_rot_deg = phantom_camera_3d.get_third_person_rotation_degrees()
+		# TODO Replace this magic number by propper mouse sensitivity setting
+		pcam_rot_deg.x -= event.relative.y * 0.1
+		# TODO replace magic number by propper min and max pitch settings
+		pcam_rot_deg.x = clamp(pcam_rot_deg.x, -89, 89)
+		pcam_rot_deg.y -= event.relative.x * 0.1
+		pcam_rot_deg.y = wrapf(pcam_rot_deg.y, -180, 180)
+		phantom_camera_3d.set_third_person_rotation_degrees(pcam_rot_deg)
--- scripts/player_rigid_3d.gd
+++ scripts/player_rigid_3d.gd
@@ -2,7 +2,10 @@ class_name PlayerRigid3D
 extends RigidBody3D
 
 var wish_dir := Vector3.ZERO
+@onready var model: Node3D = $HumanM_BasicMotionsFREE_2_6
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	state.linear_velocity += wish_dir
+	if not state.linear_velocity.is_zero_approx():
+		model.rotation.y = lerp_angle(model.rotation.y, atan2(-wish_dir.x, -wish_dir.z), 0.1)
 	wish_dir = Vector3.ZERO
