d965da72d78b75815838ab27d0bb073b25e9f3fd
CloudsCloudsClouds <56483061+CloudsCloudsClouds@users.noreply.github.com>
2025-10-30 14:14:59 -0400

    Movement finally working

--- scripts/player_nini.gd
+++ scripts/player_nini.gd
@@ -3,7 +3,6 @@ extends Node3D
 @onready var player: PlayerRigid3D = $PlayerRigid3D
 @onready var skeleton_3d: Skeleton3D = $PlayerRigid3D/HumanM_BasicMotionsFREE_2_6/Rig/Skeleton3D
 @onready var phantom_camera_3d: PhantomCamera3D = $PhantomCamera3D
-@onready var animation_tree: AnimationTree = $AnimationTree
 var bs2d := "parameters/BlendSpace2D/blend_position"
 
 enum STATE {
@@ -14,9 +13,6 @@ enum STATE {
 var state: STATE = STATE.MOVEMENT
 
 func _ready() -> void:
-	player.axis_lock_angular_x = true
-	player.axis_lock_angular_z = true
-	player.axis_lock_angular_y = true
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(_delta: float) -> void:
@@ -28,8 +24,6 @@ func _physics_process(_delta: float) -> void:
 		wish_dir = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, input_rot)
 		wish_dir = wish_dir.normalized()
 		player.wish_dir = wish_dir
-
-		animation_tree.set(bs2d, Vector2(input_dir.x, input_dir.y))
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
--- scripts/player_rigid_3d.gd
+++ scripts/player_rigid_3d.gd
@@ -6,17 +6,33 @@ extends RigidBody3D
 var wish_dir := Vector3.ZERO
 @onready var model: Node3D = $HumanM_BasicMotionsFREE_2_6
 var ground_move := true
+@onready var animation_tree: AnimationTree = $AnimationTree
+var blend_param := "parameters/BlendSpace2D/blend_position"
+
+func _ready() -> void:
+	activate_ground_mode()
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	state.linear_velocity += wish_dir
-	if not state.linear_velocity.is_zero_approx() and wish_dir != Vector3.ZERO:
-		model.rotation.y = lerp_angle(model.rotation.y, atan2(-wish_dir.x, -wish_dir.z), 0.1)
-	wish_dir = Vector3.ZERO
+	if ground_move:
+		# Move getting the direction from the player
+		state.linear_velocity += wish_dir
+		if not state.linear_velocity.is_zero_approx() and wish_dir != Vector3.ZERO:
+			model.rotation.y = lerp_angle(model.rotation.y, atan2(-wish_dir.x, -wish_dir.z), 0.1)
+		wish_dir = Vector3.ZERO
 
-	# Limit ground speed
-	var horizontal_velocity := Vector3(state.linear_velocity.x, 0, state.linear_velocity.z)
-	var horizontal_speed := horizontal_velocity.length()
-	if horizontal_speed > max_ground_speed:
-		horizontal_velocity = horizontal_velocity.normalized() * max_ground_speed
-		state.linear_velocity.x = horizontal_velocity.x
-		state.linear_velocity.z = horizontal_velocity.z
+		# Limit ground speed
+		var horizontal_velocity := Vector3(state.linear_velocity.x, 0, state.linear_velocity.z)
+		var horizontal_speed := horizontal_velocity.length()
+		if horizontal_speed > max_ground_speed:
+			horizontal_velocity = horizontal_velocity.normalized() * max_ground_speed
+			state.linear_velocity.x = horizontal_velocity.x
+			state.linear_velocity.z = horizontal_velocity.z
+		
+		# Update animation blend parameters
+		animation_tree.set(blend_param, Vector2(state.linear_velocity.x, state.linear_velocity.z))
+
+func activate_ground_mode() -> void:
+	ground_move = true
+	axis_lock_angular_x = true
+	axis_lock_angular_z = true
+	axis_lock_angular_y = true
